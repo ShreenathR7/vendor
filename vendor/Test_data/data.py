e3976b779b30c04f0bd7dcc815329f8dbd2ad914
ShreenathR7 <141302681+ShreenathR7@users.noreply.github.com>
2023-08-25 18:23:50 +0530

    Add files via upload

---
+++ vendor/Test_data/data.py
@@ -0,0 +1,73 @@
+# data.py - Keep all your Testing Data in this file
+class Logi_Data:
+    url = 'https://retail.logimaxindia.com/etail_v3/admin/index.php/customer/add'
+
+    username = 'logimax'
+
+    password = 'lmx#dev@etail'
+    
+    name = 'shankar'
+    
+    Last_name = 'A'
+    
+    mobile_NO = '9789767864'
+    
+    mail_id = 'shankar65@gmail.com'
+
+    phone_no = '9876754734'
+    
+    user_username = 'shankar'
+
+    user_password = 'shankar#*131'
+    
+    fist_Address = '23/7 jj nagar raja street chinnapalayam'  
+    
+    second_Address = '23/7 jj nagar raja street chinnapalayam'
+    
+    Third_Address = '23/7 jj nagar raja street chinnapalayam'
+    
+    Area_Text = 'Coimbatore'
+    
+    city_pincode = '641678'
+    
+    category_Name = "GOLD RING & COIN"
+    
+    Purity_value = "75.0000"
+ 
+    Product_name = 'ARAM'
+    
+    design_name = 'SOLID CHAIN'    
+    
+    Sub_design_name = 'RING & COIN'
+    
+    Touch_value = '12'
+    
+    VA ='23'
+    
+    VA_Type = 'percentage'
+    
+    V_A_pec = '12'
+
+    MC = '563'
+    
+    charge = '100'  
+    
+    From_no = '10'
+    
+    To_no = '12'
+    
+    rate_value = '500'  
+    
+    Bank_Name = "SBI"
+    
+    account_holder_name = "shakar"
+    
+    Account_no = '675345423675'
+  
+    ifsc_no = 'SBIN0000827'
+    
+    id_No = '6573456628365'
+    
+   
+    
+    
---
+++ vendor/Test_locators/locators.py
@@ -0,0 +1,173 @@
+# locators.py - File where all The HTML Locators are Kept
+class Logi_Locators:
+    username_inputBox = 'username'
+
+    password_inputBox = 'password'
+    
+   
+    signButton ='(//*[@id="submit_login"])'
+    
+    side_bar = '(//*[@role="button"])'
+    
+    retail_catalog = '(//*[@class="fa fa-angle-left pull-right"])[3]'
+   
+    purchase_module = 'Purchase Module'
+    
+    Vendor_Registration_tab = 'Vendor Registration'
+    
+    Add_karigar = 'karigar_add'
+    
+    User_for = '(//*[@role="presentation"])[1]'
+    
+    list_user_for = '(//*[@role="treeitem"])[3]'
+    
+    status_no = '(//*[@class="bootstrap-switch-handle-on bootstrap-switch-primary"])'
+    
+    status_yes = '(//*[@class="bootstrap-switch-handle-off bootstrap-switch-default"])'
+    
+    first_name = 'first_name'
+    
+    last_name = 'last_name_karigar'
+    
+    karigar_mobile = 'karigar_mobile'
+    
+    karigar_mail_id = 'email_karigar'
+    
+    karigar_phone_NO = 'phone_karigar'
+    
+    username_karigar = 'user_name_karigar'
+    
+    password_karigar = 'password'
+    
+    Address1 = 'address1'
+    
+    Address2 = 'address2_karigar'
+    
+    Address3 = 'address3'
+    
+    select_city = '(//*[@role="presentation"])[9]'
+    
+    city_text_box = '(//*[@role="textbox"])'
+    
+    pincode = 'kar_pincode'
+    
+    save = 'add_karNext'
+    
+    contract_price = '(//*[@class="vendor_det"])[1]'
+    
+    add_wastage = '//span[@class="add_wastage"]'
+    
+    category = '(//*[@role="presentation"])[12]'    
+    
+    category_list = '(//*[@role="textbox"])'
+    
+    purity = '(//*[@role="presentation"])[14]'
+    
+    purity_list = '(//*[@role="textbox"])'
+    
+    product = '(//*[@role="presentation"])[16]'
+    
+    product_list = '(//*[@role="textbox"])'   
+    
+    design = '(//*[@role="presentation"])[18]'
+    
+    design_list = '(//*[@role="textbox"])'   
+    
+    sub_design = '(//*[@role="presentation"])[20]'
+    
+    sub_design_list = '(//*[@role="textbox"])' 
+    
+    karigar_calculation = '(//*[@role="presentation"])[22]'    
+    
+    karigar_cal_type_list = '(//*[@role="treeitem"])[1]'
+    
+    Touch = '(//*[@type="number"])[3]'
+    
+    calculation = '(//*[@role="presentation"])[24]'    
+    
+    cal_type = '(//*[@role="textbox"])'
+    
+    cal_type_list = '(//*[@role="treeitem"])[2]'    
+    
+    VA_type = '(//*[@role="presentation"])[26]'    
+    
+    VA_type_list = '(//*[@role="textbox"])'
+    
+    va= '(//*[@type="number"])[4]'   
+    
+    MC_type = '(//*[@role="presentation"])[28]'    
+    
+    mc_type_lists = '(//*[@role="treeitem"])[2]'     
+    
+    MC= '(//*[@type="number"])[6]'    
+    
+    charge = '(//*[@class="btn btn-default btn-sm "])'
+    
+    charge_Name = '(//*[@class="form-control chargesType"])'
+    
+    calc_type = '//select[@class="form-control calc_type"]'
+    
+    charge_value = '(//*[@class="form-control chargesValue"])'
+    
+    save_charge = 'update_charge_details'
+    
+    save_page = 'add_contractPriceNext'
+    
+    karigar_edit = '(//*[@class="btn btn-primary btn-edit"])[1]'
+    
+    add_stone = '//span[@class="add_stone"]'
+    
+    stone_page = '(//*[@class="vendor_det"])[2]'
+    
+    stone_type = '(//*[@role="presentation"])[12]'
+    
+    text_box = '(//*[@role="textbox"])'
+    
+    stone_name = '(//*[@role="presentation"])[14]'
+    
+    uom = '(//*[@role="presentation"])[16]'
+    
+    calculation_type = '(//*[@role="presentation"])[18]'
+    
+    code = '(//*[@role="presentation"])[20]'
+    
+    From = 'stn[1][from_wt]'
+    
+    To = 'stn[1][to_wt]'
+    
+    Rate = 'stn[1][stone_rate]'
+    
+    save_stone_page = 'add_StoneNext'
+    
+    kyc = 'KYC'
+    
+    add_bank = '//span[@class="add_bank"]'    
+    
+    bank_name = 'bank_name1'
+    
+    account_holder_name = 'acc_holder1'
+    
+    account_number = 'acc_number1'
+    
+    ifsc_code = 'ifsc_code1'
+    
+    add_Kyc = '//span[@class="add_kyc"]'
+    
+    proof_name = '(//*[@role="presentation"])[12]'
+    
+    ID_no = 'kyc_det[1][id_reg_exp]'
+    
+    save_kyc = 'add_NewKyc'
+    
+    delete = '(//*[@class="btn btn-danger btn-del"])[1]'
+    
+    
+    
+    
+    
+    
+    
+      
+    
+      
+    
